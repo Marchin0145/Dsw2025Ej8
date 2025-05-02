ab88309461816bd26fcafe8952b3b28fbe4a0a1b
Lucas Campos <lucas.campos@alu.frt,utn.edu.ar>
2025-05-02 01:05:50 -0300

    Excepcion SaldoInsuficiente agregada

--- Domain/SaldoInsuficiente.cs
+++ Domain/SaldoInsuficiente.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Dsw2025Ej8.Domain
 {
-    internal class SaldoInsuficiente
+    internal class SaldoInsuficiente : Exception
     {
+        public SaldoInsuficiente(string mensaje = "La cuenta no cuenta con saldo para la operación solicitada. Fue suspendida.")
+         : base(mensaje) { }
     }
 }
