5a3f1604a53ebb2d7ac0c8b8847ed1c616a55fb0
Marchin0145 <HectorMartin.Rodriguez@alu.frt.utn.edu.ar>
2025-05-01 23:03:03 -0300

    Implementacion CajaDeAhorro

---
+++ Domain/CajaDeAhorro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Domain
+{
+    public class CajaDeAhorro:CuentaBancaria
+    {
+        public CajaDeAhorro(string numero, decimal saldo,string[] titulares ): base(numero,saldo,TipoCuenta.CajaDeAhorro, titulares){ 
+        
+        }
+        public override void Depositar(decimal monto) {
+            _saldo += monto;
+        }
+        public override void Retirar(decimal monto)
+        {
+            _saldo -= monto;
+        }
+        public void AplicarInteres()
+    {
+        if (GetTipo() == TipoCuenta.CajaDeAhorro)
+        {
+            _saldo += _saldo *GetTasaDeInteres(;
+        }
+    }
+    }
+}
