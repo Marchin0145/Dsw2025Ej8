db13a52e30ca01784f87220c2d290bc14ff63364
francotartaloUTN <Franco.TartaloAguirre@alu.frt.utn.edu.ar>
2025-05-01 23:30:55 -0300

    Reemplaza getters y setters por propiedades

--- Domain/CuentaBancaria.cs
+++ Domain/CuentaBancaria.cs
@@ -2,14 +2,14 @@
 
 public abstract class CuentaBancaria
 {
-    private TipoCuenta _tipo;
-    private string _numero;
+    public TipoCuenta _tipo { get;}
+    public string _numero { get;}
     public decimal _saldo { get; set;}
-    private Estado _estado;
-    private decimal _tasaDeInteres;
-    private decimal _limiteDeDescubierto;
-    private decimal _comision;
-    private string[] _titulares;
+    public Estado _estado { get; set;}
+    public decimal _tasaDeInteres { get; set;}
+    public decimal _limiteDeDescubierto { get; set;}
+    public decimal _comision { get; set;}
+    public string[] _titulares { get;}
 
     public CuentaBancaria(string numero, decimal saldo, TipoCuenta tipo, string[] titulares)
     {
@@ -19,66 +19,6 @@ public abstract class CuentaBancaria
         _estado = Estado.Activa;
         _titulares = titulares;
     }
-    #region Getters/Setters
-    public string GetNumero()
-    {
-        return _numero;
-    }
-
-    public decimal GetSaldo()
-    {
-        return _saldo;
-    }
-    public TipoCuenta GetTipo()
-    {
-        return _tipo;
-    }
-
-    public Estado GetEstado()
-    {
-        return _estado;
-    }
-
-    public void SetEstado(Estado estado)
-    {
-        _estado = estado;
-    }
-
-    public decimal GetTasaDeInteres()
-    {
-        return _tasaDeInteres;
-    }
-
-    public void SetTasaDeInteres(decimal tasaDeInteres)
-    {
-        _tasaDeInteres = tasaDeInteres;
-    }
-
-    public decimal GetLimiteDeDescubierto()
-    {
-        return _limiteDeDescubierto;
-    }
-
-    public void SetLimiteDeDescubierto(decimal limiteDeDescubierto)
-    {
-        _limiteDeDescubierto = limiteDeDescubierto;
-    }
-
-    public decimal GetComision()
-    {
-        return _comision;
-    }
-
-    public void SetComision(decimal comision)
-    {
-        _comision = comision;
-    }
-
-    public string[] GetTitulares()
-    {
-        return _titulares;
-    }
-    #endregion
 
     public abstract void Depositar(decimal monto);
 
