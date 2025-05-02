e3e01800e2601436622f139f8c1c0ef56c219bac
Lucas Campos <lucas.campos@alu.frt,utn.edu.ar>
2025-05-01 23:08:30 -0300

    Agregue clase CuentaBancaria

---
+++ Domain/CuentaCorriente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Dsw2025Ej8.Domain
+{
+    internal class CuentaCorriente : CuentaBancaria
+    {
+
+        public CuentaCorriente(string numero, decimal saldo, string[] titulares) : base(numero, saldo,TipoCuenta.CuentaCorriente, titulares)
+        {
+
+        }
+
+        public override void Depositar(Decimal monto)
+        {
+            monto -= monto * _comision;
+            _saldo += monto;
+        }
+
+        public override void Retirar(decimal monto) {
+
+            if (_saldo - monto >= -_limiteDeDescubierto)
+            {
+                _saldo -= monto;
+            }
+            if (_saldo < 0)
+            {
+                _estado = Estado.Suspendida;
+            }
+        }
+
+}
